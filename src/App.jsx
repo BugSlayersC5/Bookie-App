0fd5f60c4b1a36c535ae898d7b81ca8d6b2b595e
jessicaaning <jersycaowusu@gmail.com>
2025-06-09 11:40:26 +0000

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Welcome to Bookie-App</h1>
+      <h1 className='font-bold text-purple-800'>Welcome to Bookie-App</h1>
     </>
   )
 }
