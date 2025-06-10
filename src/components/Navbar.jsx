c7296f0fa01b43b97dfe1535031aa5b1d22d2de4
Akosua Anima <akosuaanima73@gmail.com>
2025-06-10 13:27:49 +0000

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
+import Logo from "../assets/bookielogo.png";
 export default function Navbar(){
 
     return(
-        <>
-            <h1>Navbar</h1>
-        </>
+        <nav className=" bg-gray-800 py-4 px-6 shadow-sm flex  flex-row">
+            <div className="flex   mx-auto  ">
+            <img src={Logo} alt="" />
+            </div>
+            <div className="flex space-x-4 items-center ">
+                <a href="http://" className=" hover:text-yellow-500 font-medium text-white"> Home</a>
+                <a href="http://" className=" hover:text-yellow-500 font-medium text-white">Books </a>
+                <a href="http://" className=" hover:text-yellow-500 font-medium text-white"> About</a>
+                <a href="http://" className=" hover:text-yellow-500 font-medium text-white">Add Book</a>
+
+            </div>  
+            
+                
+        </nav>
+        
+        
+           
     );
 }
