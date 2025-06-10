8f207985ae9dad86a1730c4c20fa2c2dd53d808c
Akosua Anima <akosuaanima73@gmail.com>
2025-06-10 15:55:43 +0000

    working on the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,27 @@
-export default function Footer(){
-    return(
-        <div>
-            <h1>Footer</h1>
-        </div>
+import Logo from "../assets/bookielogo.png";
+import { Linkedin, Instagram, Facebook ,Twitter } from "lucide-react";
+
+
+export default function Footer() {
+    return (
+        <footer className="bg-[#234046] py-12 px-7 text-white">
+            <div className="mb-3">
+                <img src={Logo} alt="" />
+            </div>
+            <div>
+                <p className="font-medium">Library collection with ease. <br /> Search,add,edit and organize your books. <br />Great Book,Smart System </p>
+            </div>
+
+            <div className="flex flex-row space-x-2 mt-5">
+              <Linkedin className="hover:text-[#FDD36B]"/>
+              <Instagram className="hover:text-[#FDD36B]"/>
+              <Facebook className="hover:text-[#FDD36B]"/>
+              <Twitter className="hover:text-[#FDD36B]"/>
+            </div>
+
+        </footer>
+
+
 
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
+import { Link } from "react-router";
 import Logo from "../assets/bookielogo.png";
 export default function Navbar(){
 
     return(
-        <nav className=" bg-gray-800 py-4 px-6 shadow-sm flex  flex-row">
-            <div className="flex   mx-auto  ">
+        <nav className=" bg-[#234046] py-4 px-6 shadow-sm flex  flex-row">
+            <div className="flex   mx-auto ml-19  ">
             <img src={Logo} alt="" />
             </div>
-            <div className="flex space-x-4 items-center ">
-                <a href="http://" className=" hover:text-yellow-500 font-medium text-white"> Home</a>
-                <a href="http://" className=" hover:text-yellow-500 font-medium text-white">Books </a>
-                <a href="http://" className=" hover:text-yellow-500 font-medium text-white"> About</a>
-                <a href="http://" className=" hover:text-yellow-500 font-medium text-white">Add Book</a>
+            <div className="flex space-x-8 items-center ">
+                <Link to="/" className=" hover:text-[#FDD36B] font-medium text-white"> Home</Link>
+                <Link to="/view-book" className=" hover:text-[#FDD36B] font-medium text-white">Books </Link>
+                <Link to="/create-book" className=" hover:text-[#FDD36B] font-medium text-white">Add Book</Link>
 
             </div>  
             
